e1f6a0b8a367cba5a8c7087bacb2f65648fccbaf
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-08-09 08:01:04 -0300

    Create Main5.java

---
+++ Main5.java
@@ -0,0 +1,15 @@
+// Código que verifica par ou impar
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	      int numero;
+	      numero = 4;
+	      
+	       if(numero % 2 == 0){
+	           System.out.println("O número é par");
+	       }else{
+	           System.out.println("O número é impar");
+	       }
+	}
+}
