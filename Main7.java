ae1128c5c7e816221c674a624c7638a8c9759aa9
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-08-09 08:52:45 -0300

    Update Main7.java

--- Main7.java
+++ Main7.java
@@ -7,20 +7,27 @@ public class Main
 	      
 	      int salario;
 	      float bonus;
+	      float total;
 	      
 	      System.out.println("Digite seu salário mensal: ");
 	      salario = scan.nextInt();
 	      
 	      if(salario<1000){
 	          bonus = (salario/100)*10;
+	          total = salario + bonus;
 	          System.out.println("Seu bonus é de: R$"+bonus);
+	          System.out.println("Seu total é: R$"+total);
 	      }else if(salario >=1000 && salario <=2000){
 	          bonus = (salario/100)*5;
+	          total = salario + bonus;
 	          System.out.println("Seu bonus é de: R$"+bonus);
+	          System.out.println("Seu total é: R$"+total);
 	      }
 	      else{
 	          bonus = (salario/100)*2;
+	          total = salario + bonus;
 	          System.out.println("Seu bonus é de: R$"+bonus);
+	          System.out.println("Seu total é: R$"+total);
 	      }
 	}
 }
