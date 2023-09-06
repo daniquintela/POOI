fef4f57c91497f7c594606f31e47bcaa62d11fe3
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-09-06 08:35:28 -0300

    Create Aluno.java

---
+++ class3/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    
+    private String matricula;
+    private String nome;
+    String curso;
+    String turma;
+    int periodo;
+    double nota1B;
+    double nota2B;
+    double notaF;
+    int static qntAlunos;
+    
+}
