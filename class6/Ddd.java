fa9cff42727d984198a7d41055ecc0feab20d6fe
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-09-27 08:29:01 -0300

    Create Ddd.java

---
+++ class6/Ddd.java
@@ -0,0 +1,13 @@
+public class Ddd {
+    private String numero;
+    private String estado;
+    
+    public Ddd(String numero, String estado){
+        this.numero = numero;
+        this.estado = estado;
+    }
+    
+    public String getNumero(){
+        return numero;
+    }
+}
