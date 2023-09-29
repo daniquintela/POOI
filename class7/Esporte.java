a9a8ac11f4eb014769a9bbc5141ad8d8e865136f
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-09-29 10:38:17 -0300

    Create Esporte.java

---
+++ class7/Esporte.java
@@ -0,0 +1,26 @@
+public class Esporte {
+    
+    private String nome;
+    private int quantidade;
+    
+    public Esporte(String nome, int quantidade){
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public int getQuantidade(){
+        return quantidade;
+    }
+    
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+}
