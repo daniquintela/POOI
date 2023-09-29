12e162ce44d6e364f13f6e1f4ad000c500e445f2
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-09-29 10:38:34 -0300

    Create Atleta.java

---
+++ class7/Atleta.java
@@ -0,0 +1,25 @@
+public class Atleta {
+    private String nome;
+    private Esporte e; 
+    
+    
+    public Atleta(String nome, Esporte e){
+        
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public getE(){
+        return e;
+    }
+    
+    public void setE(Esporte e){
+        this.e = e;
+    }
+}
