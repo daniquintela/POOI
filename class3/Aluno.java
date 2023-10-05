a1ae815488e1c38531f8bc35c9c38c855f7770a4
daniquintela <125465586+daniquintela@users.noreply.github.com>
2023-10-05 18:39:42 -0300

    Update Aluno.java

--- class3/Aluno.java
+++ class3/Aluno.java
@@ -1,13 +1,56 @@
+import java.util.*;
+import java.time.Year;
+
 public class Aluno {
-    
     private String matricula;
-    private String nome;
+    String nome;
     String curso;
-    String turma;
+    char turma;
     int periodo;
-    double nota1B;
-    double nota2B;
-    double notaF;
-    int static qntAlunos;
-    
+    double nota1b;
+    double nota2b;
+    double notaf;
+    static int qntalunos;
+    Scanner scan = new Scanner(System.in);
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    Aluno(){
+        qntalunos++;
+    }
+
+    Aluno(String nome, int periodo){
+        qntalunos++;
+    }
+
+    void imprimir(){
+        System.out.printf("Nome do aluno: " + this.nome);
+        System.out.printf("\nCurso: %s", this.curso);
+        System.out.printf("\nTurma: " + this.turma);
+        System.out.printf("\nNota final: %.2f\n", this.notaf);
+    }
+
+    void mediaF(){
+        this.notaf = nota1b + nota2b / 2;
+    }
+
+    void passar(){
+        this.periodo++;
+        System.out.printf("\nO aluno %s passou para o periodo %d, em qual turma vai ser alocado: ", this.nome, this.periodo);
+        this.turma = scan.nextLine().charAt(0);
+    }
+
+    int gerarMatricula(){
+        int anoAtual = Year.now().getValue();
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(10000);
+        int m = anoAtual * 10000 + numeroAleatorio;
+        return m;
+    }
 }
